a23702f4fec710f8060a5b98c58fe30ebbed4d63
NoumanLF <bcsf19a017@pucit.edu.pk>
2022-11-17 04:12:19 -0800

    Modify

--- frontend/src/Pages/MainPage.jsx
+++ frontend/src/Pages/MainPage.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import About from '../Components/About'
+import Contact from '../Components/Contact'
+import Project from '../Components/Project'
 
+const Container = styled.div`
+    display : flex;
+    flex-direction : column;
+    justify-content : space-between;
+    background-color: #161414;
+    width : 100vw;
+    height : 100vh
+`
+const Title = styled.div`
+    display : flex;
+    flex-direction : column;
+    justify-content : center;
+    padding : 30px;
+    color  : white; 
+    height : 30vh;
+    p{
+        margin : 0;
+        font-size : 4.7rem;
+        font-weight : bold
+    }
+   
+`
+const Content = styled.div`
+    box-sizing : border-box;
+    padding : 20px;
+    height : 65vh;
+    display: grid;
+    grid-template-columns : repeat(12 , 1fr);
+    grid-template-rows : 35% 60%;
+    grid-gap : 20px 
+`
 export default function MainPage() {
   return (
-    <div>
-        <Link to ='/about'>ABout</Link>
-        <Link to ='/projects'>Project</Link>
-        <Link to ='/contact'>Contact</Link>
-    </div>
+    <Container>
+        <Title>
+            <p>Muhammad Nouman Ali</p>
+        </Title>
+        <Content>
+            <About/>
+            <Project/>
+            <Contact/>
+        </Content>
+    </Container>
+   
   )
 }
